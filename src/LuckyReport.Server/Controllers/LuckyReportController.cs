3b5e66de2819f0d920a870fb6dba2fa77cd11ed5
master never down <13126506430@163.com>
2022-11-18 10:03:16 +0800

    feat: 增加文档自动保存功能；

--- src/LuckyReport.Server/Controllers/LuckyReportController.cs
+++ src/LuckyReport.Server/Controllers/LuckyReportController.cs
@@ -31,10 +31,22 @@ namespace LuckyReport.Server.Controllers
         [HttpPost("/reports", Name = nameof(Post))]
         public async Task<bool> Post([FromBody][Required] Report report)
         {
-            await using var db = new LuckyReportContext();
-            var r =await db.Reports.AddAsync(report);
-            var c = await db.SaveChangesAsync();
-            return c == 1;
+            try
+            {
+                report.Title = "new";
+                await using var db = new LuckyReportContext();
+                var r = await db.Reports.Where(r => r.Id.Equals(report.Id)).FirstOrDefaultAsync();
+                r.Doc = report.Doc;
+                var c = await db.SaveChangesAsync();
+                Console.WriteLine(c==1);
+                return c == 1;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+           
         }
 
         [HttpGet("/reports", Name = nameof(GetAll))]
@@ -45,12 +57,12 @@ namespace LuckyReport.Server.Controllers
             return r;
         }
 
-        [HttpGet("/reports/{id}", Name = nameof(Get))]
-        public async Task<Report?> Get([FromRoute][Required] int id)
+        [HttpPost("/reports/{id}", Name = nameof(Get))]
+        public async Task<string> Get([FromRoute][Required] int id)
         {
             await using var db = new LuckyReportContext();
             var r =await db.Reports.Where(r=>r.Id.Equals(id)).FirstOrDefaultAsync();
-            return r;
+            return r.Doc;
         }
     }
 }
