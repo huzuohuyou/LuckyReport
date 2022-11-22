484f7a32361986c1d8ca92e72c88d40bf055442c
WIN-MDI085JTSLE\Administrator <13126506430@163.com>
2022-11-22 11:06:12 +0800

    docs: 数据库，swagger聚合

--- src/LuckyReport.Server/Model.cs
+++ src/LuckyReport.Server/Model.cs
@@ -14,7 +14,7 @@ public class LuckyReportContext : DbContext
     {
         var folder = Environment.SpecialFolder.LocalApplicationData;
         var path = Environment.GetFolderPath(folder);
-        DbPath = System.IO.Path.Join(path, "luckyreport.db");
+        DbPath = System.IO.Path.Join( "luckyreport.db");
     }
 
     // The following configures EF to create a Sqlite database file in the
